fbc6cea4172fa467e2b8e32d518bc85fff1b6e4f
createsomethings <126850742+createsomethings@users.noreply.github.com>
2023-03-07 12:06:41 +1100

    Add files via upload

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -2,9 +2,9 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription(
+    return LaunchDescription([
         Node(
             package='demo_nodes_cpp',
             executable='listener'
         )
-    )
+    ])
